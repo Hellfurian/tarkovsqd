b340d2ea5dcad7b383ffa391a25d590540cbc8bf
Hellfurian <Tyler.Long1228@gmail.com>
2023-10-06 11:11:14 -0400

    Update Main.jsx
    Add play type

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,23 +1,23 @@
-import { 
-    Box, 
-    Container, 
-    HStack, 
-    Button,
-    Table,
-    Thead,
-    Tbody,
-    Tr,
-    Th,
-    Td,
-    TableContainer,
-    Avatar,
-    VStack,
-    Text,
-    useDisclosure,
-    Image,
-    useToast,
-    Tooltip,
-    Center
+import {
+     Box,
+     container,
+     HStack,
+     Button,
+     Table,
+     Thead,
+     body,
+     Tr,
+     Th,
+     td,
+     TableContainer,
+     Avatar,
+     VStack,
+     Text,
+     useDisclosure;
+     Image,
+     useToast,
+     Tooltips,
+     Center
 } from "@chakra-ui/react";
 
 import Header from "../components/Header";
@@ -47,168 +47,148 @@ import { API_SERVER } from "../application";
 import axios from "axios";
 import useFilterStore from "../store/useFilterStore";
 
-export default function Main () {
+export default function Main() {
 
-    const toast = useToast()
-    const loginModal = useDisclosure()
-    const teamModal = useDisclosure()
+     const toast = useToast()
+     const loginModal = useDisclosure()
+     const teamModal = useDisclosure()
 
-    const {posts, initPosts}  = usePostStore()  
-    const {map, server, type} = useFilterStore()
+     const {posts, initPosts} = usePostStore()
+     const {map, server, type} = useFilterStore()
 
-    const openModal = () => {
-        const token = localStorage.getItem('squadObject')
+     const openModal = () => {
+         const token = localStorage.getItem('squadObject')
 
-        if (token === null) {
-            loginModal.onOpen()
-            return
-        }
+         if (token === null) {
+             loginModal.onOpen()
+             return
+         }
 
-        teamModal.onOpen()
-    }
+         teamModal.onOpen()
+     }
 
-    const handleCopyClipBoard = (e) => {
-        try {
-            navigator.clipboard.writeText(e.target.innerHTML)
-            toast({
-                title: '디스코드 아이디가 복사되었어요',
-                status: 'info'
-            })
-        } 
-        catch (error) { alert('클립보드 복사에 실패하였습니다.') }
-    };
+     const handleCopyClipBoard = (e) => {
+         try {
+             navigator.clipboard.writeText(e.target.innerHTML)
+             toast({
+                 title: ‘Discord ID has been copied’,
+                 status: 'info'
+             })
+         }
+         catch (error) { alert('Clipboard copy failed.') }
+     };
 
-    const searchQueryGenerator = () => {
-        const query = new URLSearchParams()
-        if (map !== null) { query.append("map", map) }
-        if (server !== null) { query.append("server", server) }
-        if (type !== null) { query.append("type", type) }
-        return query.toString()
-    }
+     const searchQueryGenerator = () => {
+         const query = new URLSearchParams()
+         if (map !== null) { query.append("map", map) }
+         if (server !== null) { query.append("server", server) }
+         if (type !== null) { query.append("type", type) }
+         return query.toString()
+     }
 
-    const onResetFilterClick = () => {
-        axios.get(API_SERVER + '/post').then(res => { initPosts(res.data.content) })
-    }
+     const onResetFilterClick = () => {
+         axios.get(API_SERVER + '/post').then(res => { initPosts(res.data.content) })
+     }
     
-    useEffect(() => {
-        console.log(API_SERVER + '/post?' + searchQueryGenerator())
-        axios.get(API_SERVER + '/post?' + searchQueryGenerator()).then(res => { initPosts(res.data.content) })
-    }, [map, server, type])
+     useEffect(() => {
+         console.log(API_SERVER + '/post?' + searchQueryGenerator())
+         axios.get(API_SERVER + '/post?' + searchQueryGenerator()).then(res => { initPosts(res.data.content) })
+     }, [map, server, type])
 
-    return (
-    <Box w={'100%'} bgColor={'black'} backgroundImage={bg} backgroundSize={'cover'} backgroundRepeat={'no-repeat'} backgroundAttachment={'fixed'} pb={'400px'}>
-        <LoginModal onClose={loginModal.onClose} isOpen={loginModal.isOpen} teamModalOpen={teamModal.onOpen} />
-        <TeamCreateModal onClose={teamModal.onClose} isOpen={teamModal.isOpen} />
-        <Header />
-        <Container maxW={'1200px'} mt={8}>
-            {/* <Box w={'100%'} p={'18px 24px 18px 24px'} bgColor={'#151515'} border={'1px solid #9a886650'}>
-                <VStack alignItems={'flex-start'} spacing={3}>
-                    <Text fontSize={'18px'} letterSpacing={'-1px'} fontWeight={'bold'} color={'#9A8866'}>공지사항</Text>
-                    <Text fontSize={'15px'}>
-                        필터링 기능(맵, 서버)이 추가되었습니다. <br/>
-                        사용자 프로필을 클릭하면 빠르게 디스코드 아이디를 복사할 수 있습니다. <br/>
-                    </Text>
-                </VStack>
-            </Box> */}
-            <HStack justifyContent={'space-between'} pt={5}>
-                <HStack spacing={4}>
-                    <Tooltip fontSize={'12px'} label={'필터 초기화'}>
-                        <Box bgColor={'#121211'} border={'1px solid #9A8866'} cursor={'pointer'} onClick={onResetFilterClick}>
-                            <Image p={'6px'} src={reset} h={'38px'} w={'39px'} />
-                        </Box>
-                    </Tooltip>
-                    <MapSelector      isFilter={true} />
-                    <PlayTypeSelector isFilter={true} />
-                    <RegionSelector   isFilter={true} />
-                </HStack>
-                <Button fontSize={'14px'} bgColor={'#9A8866'} color={'#29241D'} borderRadius={0} fontWeight={'bold'} letterSpacing={'-1px'} onClick={openModal}>팀원 찾기</Button>
-            </HStack>
-            <Box bgColor={'#171715'} mt={6}>
-                <TableContainer w={'100%'}>
-                    <Table>
-                        <Thead bgImage={strip}>
-                            <Tr>
-                                <Th textShadow={'1px 1px 1px rgba(0,0,0,.004)'} w={'250px'} fontSize={'11px'} color={'#9A8866'}>디스코드</Th>
-                                <Th textShadow={'1px 1px 1px rgba(0,0,0,.004)'} w={'180px'} fontSize={'11px'} color={'#9A8866'}>맵</Th>
-                                <Th textShadow={'1px 1px 1px rgba(0,0,0,.004)'} w={'180px'} fontSize={'11px'} color={'#9A8866'}>플레이 유형</Th>
-                                <Th textShadow={'1px 1px 1px rgba(0,0,0,.004)'} w={'200px'} color={'#9A8866'}>서버</Th>
-                                <Th textShadow={'1px 1px 1px rgba(0,0,0,.004)'} fontSize={'11px'} color={'#9A8866'} w={'450px'}>메모</Th>
-                                <Th textShadow={'1px 1px 1px rgba(0,0,0,.004)'} fontSize={'11px'} color={'#9A8866'}>등록 시간</Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            {posts && posts.map(p => 
-                            <Tr fontSize={'12px'} bgColor={'#151515'}>
-                                <Td>
-                                    <HStack spacing={2}>
-                                        <Avatar size={'sm'} src={p.avatar_url} />
-                                        <VStack spacing={0} alignItems={'flex-start'}>
-                                            <Tooltip fontSize={'10px'} label={'아이디 복사'}>
-                                                <Text color={'#9A8866'} cursor={'pointer'} onClick={handleCopyClipBoard}>{p.nickname}</Text>
-                                            </Tooltip>
-                                            {p.verify === true ?  <Text color={'#5865f2'} fontSize={'10px'} letterSpacing={'-1px'}>디스코드 인증됨</Text>
-                                            : <Text color={'gray'} fontSize={'10px'} letterSpacing={'-1px'}>디스코드 미인증</Text>}
-                                        </VStack>
-                                    </HStack>
-                                </Td>
+     return (
+     <Box w={'100%'} bgColor={'black'} backgroundImage={bg} backgroundSize={'cover'} backgroundRepeat={'no-repeat'} backgroundAttachment={'fixed'} pb={'400px '}>
+         <LoginModal onClose={loginModal.onClose} isOpen={loginModal.isOpen} teamModalOpen={teamModal.onOpen} />
+         <TeamCreateModal onClose={teamModal.onClose} isOpen={teamModal.isOpen} />
+         <Header />
+         <Container maxW={'1200px'} mt={8}>
+             {/* <Box w={'100%'} p={'18px 24px 18px 24px'} bgColor={'#151515'} border={'1px solid #9a886650'}>
+                 <VStack alignItems={'flex-start'} spacing={3}>
+                     <Text fontSize={'18px'} letterSpacing={'-1px'} fontWeight={'bold'} color={'#9A8866'}>Notice</Text>
+                     <Text fontSize={'15px'}>
+                         Filtering functions (map, server) have been added. <br/>
+                         You can quickly copy your Discord ID by clicking on your user profile. <br/>
+                     </Text>
+                 </VStack>
+             </Box> */}
+             <HStack justifyContent={'space-between'} pt={5}>
+                 <HStack spacing={4}>
+                     <Tooltip fontSize={'12px'} label={'Reset Filter'}>
+                         <Box bgColor={'#121211'} border={'1px solid #9A8866'} cursor={'pointer'} onClick={onResetFilterClick}>
+                             <Image p={'6px'} src={reset} h={'38px'} w={'39px'} />
+                         </Box>
+                     </Tooltip>
+                     <MapSelector isFilter={true} />
+                     <PlayTypeSelector isFilter={true} />
+                     <RegionSelector isFilter={true} />
+                 </HStack>
+                 <Button fontSize={'14px'} bgColor={'#9A8866'} color={'#29241D'} borderRadius={0} fontWeight={'bold'} letterSpacing={'-1px'} onClick={openModal} >Find team members</Button>
+             </HStack>
+             <Box bgColor={'#171715'} mt={6}>
+                 <TableContainer w={'100%'}>
+                     <Table>
+                         <Thead bgImage={strip}>
+                             <Tr>
+                                 <Th textShadow={'1px 1px 1px rgba(0,0,0,.004)'} w={'250px'} fontSize={'11px'} color={'#9A8866'}>Discord</Th >
+                                 <Th textShadow={'1px 1px 1px rgba(0,0,0,.004)'} w={'180px'} fontSize={'11px'} color={'#9A8866'}>map</Th>
+                                 <Th textShadow={'1px 1px 1px rgba(0,0,0,.004)'} w={'200px'} color={'#9A8866'}>Server</Th>
+                                 <Th textShadow={'1px 1px 1px rgba(0,0,0,.004)'} fontSize={'11px'} color={'#9A8866'} w={'450px'}>Memo</Th>
+                                 <Th textShadow={'1px 1px 1px rgba(0,0,0,.004)'} fontSize={'11px'} color={'#9A8866'}>Registration time</Th>
+                             </Tr>
+                         </Thead>
+                         <Tbody>
+                             {posts && posts.map(p =>
+                             <Tr fontSize={'12px'} bgColor={'#151515'}>
+                                 <Td>
+                                     <HStack spacing={2}>
+                                         <Avatar size={'sm'} src={p.avatar_url} />
+                                         <VStack spacing={0} alignItems={'flex-start'}>
+                                             <Tooltip fontSize={'10px'} label={'Copy ID'}>
+                                                 <Text color={'#9A8866'} cursor={'pointer'} onClick={handleCopyClipBoard}>{p.nickname}</Text>
+                                             </Tooltip>
+                                             {p.verify === true ? <Text color={'#5865f2'} fontSize={'10px'} letterSpacing={'-1px'}>Discord Verified</Text>
+                                             : <Text color={'gray'} fontSize={'10px'} letterSpacing={'-1px'}>Discord not authenticated</Text>}
+                                         </VStack>
+                                     </HStack>
+                                 </Td>
 
-                                <Td color={'#AEAEB0'} fontWeight={'bold'}>{p.map}</Td>
+                                 <Td color={'#AEAEB0'} fontWeight={'bold'}>{p.map}</Td>
                                 
-                                {p.type === "" && <Td color={'gray'}>미지정</Td>}
-                                {p.type === "파밍" && 
-                                    <Td color={'gold'}>
-                                        <Tooltip bgColor={'#9a8866'} color={'#373128'} fontSize={'12px'} label={'파밍 위주 플레이'}>
-                                            <Image ml={2} src={money} h={'24px'} w={'24px'} filter={'opacity(0.5) drop-shadow(0 0 0 yellow)'}/></Tooltip></Td>}
-                                {p.type === "보스런" && 
-                                    <Td color={'crimson'}>
-                                        <Tooltip bgColor={'#9a8866'} color={'#373128'} fontSize={'12px'} label={'보스만 목표로 달린다'}>
-                                            <Image ml={2} src={boss} h={'24px'} w={'26px'} filter={'opacity(0.5) drop-shadow(0 0 0 red)'}/></Tooltip></Td>}
-                                {p.type === "교전" &&
-                                    <Td color={'crimson'}>
-                                        <Tooltip bgColor={'#9a8866'} color={'#373128'} fontSize={'12px'} label={'교전 위주 플레이'}>
-                                            <Image ml={2} src={gun} h={'24px'} w={'24px'} filter={'opacity(0.5) drop-shadow(0 0 0 red)'}/></Tooltip></Td>}
-                                {p.type === "퀘스트" &&
-                                    <Td color={'crimson'}>
-                                        <Tooltip bgColor={'#9a8866'} color={'#373128'} fontSize={'12px'} label={'퀘스트 위주 플레이'}>
-                                            <Image ml={2} src={quest} h={'24px'} w={'24px'} filter={'opacity(0.5) drop-shadow(0 0 0 #9a8866)'}/></Tooltip></Td>}
+                                 {p.type === "" && <Td color={'gray'}>Unspecified</Td>}
+                                 {p.type === "Farming" &&
+                                     <Td color={'gold'}>
+                                         <Tooltip bgColor={'#9a8866'} color={'#373128'} fontSize={'12px'} label={'Farming-focused play'}>
+                                             <Image ml={2} src={money} h={'24px'} w={'24px'} filter={'opacity(0.5) drop-shadow(0 0 0 yellow)'}/></Tooltip ></Td>}
+                                 {p.type === "Boss run" &&
+                                     <Td color={'crimson'}>
+                                         <Tooltip bgColor={'#9a8866'} color={'#373128'} fontSize={'12px'} label={'Aim only for the boss'}>
+                                             <Image ml={2} src={boss} h={'24px'} w={'26px'} filter={'opacity(0.5) drop-shadow(0 0 0 red)'}/></Tooltip ></Td>}
+                                 {p.type === "Engagement" &&
+                                     <Td color={'crimson'}>
+                                         <Tooltip bgColor={'#9a8866'} color={'#373128'} fontSize={'12px'} label={'Combat-oriented play'}>
+                                             <Image ml={2} src={gun} h={'24px'} w={'24px'} filter={'opacity(0.5) drop-shadow(0 0 0 red)'}/></Tooltip ></Td>}
+                                 {p.type === "Quest" &&
+                                     <Td color={'crimson'}>
+                                         <Tooltip bgColor={'#9a8866'} color={'#373128'} fontSize={'12px'} label={'Quest-oriented play'}>
+                                             <Image ml={2} src={quest} h={'24px'} w={'24px'} filter={'opacity(0.5) drop-shadow(0 0 0 #9a8866)'}/></ Tooltip></Td>}
         
-                                {p.server === '한국' && <Td>
-                                    <HStack><Image w={'18px'} h={'18px'} src={kr} /><Text color={'#AEAEB0'}>{p.server}</Text></HStack></Td>}
-                                {p.server === '북미' && <Td color={'#AEAEB0'}>
-                                    <HStack><Image w={'18px'} h={'18px'} src={us} /><Text color={'#AEAEB0'}>{p.server}</Text></HStack></Td>}
-                                {p.server === '일본' && <Td color={'#AEAEB0'}>
-                                    <HStack><Image w={'18px'} h={'18px'} src={jp} /><Text color={'#AEAEB0'}>{p.server}</Text></HStack></Td>}
-                                {p.server === '러시아' && <Td color={'#AEAEB0'}>
-                                    <HStack><Image w={'18px'} h={'18px'} src={rs} /><Text color={'#AEAEB0'}>{p.server}</Text></HStack></Td>}
-                                {p.server ===  '상관없음' && <Td color={'#AEAEB0'}>
-                                    <HStack><Text color={'#AEAEB0'}>{p.server}</Text></HStack></Td>}
+                                 {p.server === 'Korea' && <Td>
+                                     <HStack><Image w={'18px'} h={'18px'} src={kr} /><Text color={'#AEAEB0'}>{p.server}</Text></HStack> </Td>}
+                                 {p.server === 'North America' && <Td color={'#AEAEB0'}>
+                                     <HStack><Image w={'18px'} h={'18px'} src={us} /><Text color={'#AEAEB0'}>{p.server}</Text></HStack> </Td>}
+                                 {p.server === 'Japan' && <Td color={'#AEAEB0'}>
+                                     <HStack><Image w={'18px'} h={'18px'} src={jp} /><Text color={'#AEAEB0'}>{p.server}</Text></HStack> </Td>}
+                                 {p.server === 'Russia' && <Td color={'#AEAEB0'}>
+                                     <HStack><Image w={'18px'} h={'18px'} src={rs} /><Text color={'#AEAEB0'}>{p.server}</Text></HStack> </Td>}
+                                 {p.server === 'Doesn't matter' && <Td color={'#AEAEB0'}>
+                                     <HStack><Text color={'#AEAEB0'}>{p.server}</Text></HStack></Td>}
 
-                                <Td wordBreak={'break-all'} pr={10}>
-                                    <Text 
-                                        color={'#9a8866'}  
-                                        whiteSpace={'normal'} 
-                                        fontWeight={'bold'}
-                                        style={{
-                                            overflow:'hidden',
-                                            display: "-webkit-box",
-                                            WebkitLineClamp: 2,
-                                            WebkitBoxOrient: 'vertical',
-                                            textOverflow: 'ellipsis',
-                                        }}>{p.memo}</Text>
-                                </Td>
-                                <Td color={'gray'}>{p.time}</Td>
-                            </Tr>)}
-                        </Tbody>
-                    </Table>
-                </TableContainer>
-                {posts && posts.length <= 0 ? 
-                    <>
-                        <Center h={'100%'} p={'200px 0 200px'}>
-                            <Text fontSize={'24px'} color={'#9A8866'} letterSpacing={'-1px'}>아무런 데이터가 없음</Text>
-                        </Center>
-                    </> : null}
-            </Box>
-        </Container>
-    </Box>
-    )
-}
+                                 <Td wordBreak={'break-all'} pr={10}>
+                                     <Text
+                                         color={'#9a8866'}
+                                         whiteSpace={'normal'}
+                                         fontWeight={'bold'}
+                                         style={{
+                                             overflow:'hidden',
+                                             display: "-webkit-box",
+                                             WebkitLineClamp: 2;
+                                             WebkitBoxOrient: 'vertical'
