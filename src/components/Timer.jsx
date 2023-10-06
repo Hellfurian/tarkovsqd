4ce45a439fba179146d818c2f7d6c7276ffb74af
Hellfurian <Tyler.Long1228@gmail.com>
2023-10-06 11:07:30 -0400

    Update Timer.jsx
    Added filtering function

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,39 +1,38 @@
-
 import { Text } from "@chakra-ui/react"
 import { useEffect, useState } from "react"
 
 export default function Timer ({time}) {
 
-    const [txt, setTxt] = useState();
+     const [txt, setTxt] = useState();
 
-    const apiTime = new Date(time);
-    useEffect(() => {
-        setInterval(() => {
-            const currentTime = new Date();
-            const diff = currentTime - apiTime;
+     const apiTime = new Date(time);
+     useEffect(() => {
+         setInterval(() => {
+             const currentTime = new Date();
+             const diff = currentTime - apiTime;
 
-            const elapsedSeconds = Math.floor(diff / 1000);
+             const elapsedSeconds = Math.floor(diff / 1000);
     
-            if (elapsedSeconds < 60) {
-                setTxt(elapsedSeconds + ' 초 전')
-            }
-            else if (elapsedSeconds < 3600) {
-                const min = Math.floor(elapsedSeconds / 60)
-                setTxt(min + ' 분 전')
-            }
-            else {
-                const hour = Math.floor(elapsedSeconds / 3600)
-                setTxt(hour + ' 시간 전')
-            }
-        }, 1000)
-    }, [])
+             if (elapsedSeconds < 60) {
+                 setTxt(elapsedSeconds + 'seconds ago')
+             }
+             else if (elapsedSeconds < 3600) {
+                 const min = Math.floor(elapsedSeconds / 60)
+                 setTxt(min + 'minutes ago')
+             }
+             else {
+                 const hour = Math.floor(elapsedSeconds / 3600)
+                 setTxt(hour + 'hour ago')
+             }
+         }, 1000)
+     }, [])
 
-    return (
-        <>
-            {time &&         
-                <Text color={'gray'}>
-                    {txt}
-                </Text>}
-        </>
-    )
+     return (
+         <>
+             {time &&
+                 <Text color={'gray'}>
+                     {txt}
+                 </Text>}
+         </>
+     )
 }
