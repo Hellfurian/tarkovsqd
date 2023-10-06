1868b809739754e1ec78fea6bd3ecd0dc754b78f
Hellfurian <Tyler.Long1228@gmail.com>
2023-10-06 11:00:14 -0400

    Update MapSelector.jsx
    Added filtering function

--- src/components/MapSelector.jsx
+++ src/components/MapSelector.jsx
@@ -1,37 +1,36 @@
-
 import { Box, Container, HStack, Select, Text } from "@chakra-ui/react";
 import { useSearchParams } from "react-router-dom";
 import useFilterStore from "../store/useFilterStore";
 
 export default function MapSelector ({setter, isFilter=false}) {
 
-    const {setMap} = useFilterStore()
+     const {setMap} = useFilterStore()
 
-    if (!isFilter) {
-        return (
-            <Select onChange={setter} fontWeight={'bold'} bgColor={'#171715'} color={'#9a8866'} borderColor={'#9a8866'} borderRadius={0} w={'150px'}  fontSize={'14px'}>
-                <option value={'맵 선택'}>맵 선택</option>
-                <option value={'전체'}>전체</option>
-                <option value={'커스텀'}>커스텀</option>
-                <option value={'팩토리'}>팩토리</option>
-                <option value={'우드'}>우드</option>
-                <option value={'스트리트'}>스트리트</option>
-                <option value={'쇼어라인'}>쇼어라인</option>
-                <option value={'연구실'}>연구실</option>
-            </Select>
-        )
-    }
+     if (!isFilter) {
+         return (
+             <Select onChange={setter} fontWeight={'bold'} bgColor={'#171715'} color={'#9a8866'} borderColor={'#9a8866'} borderRadius={0} w={'150px'} fontSize={'14px'}>
+                 <option value={'Select map'}>Select map</option>
+                 <option value={'All'}>All</option>
+                 <option value={'custom'}>Custom</option>
+                 <option value={'Factory'}>Factory</option>
+                 <option value={'Wood'}>Wood</option>
+                 <option value={'Street'}>Street</option>
+                 <option value={'Shoreline'}>Shoreline</option>
+                 <option value={'Lab'}>Laboratory</option>
+             </Select>
+         )
+     }
 
-    return (
-        <Select onChange={(e) => setMap(e.target.value)} fontWeight={'bold'} bgColor={'#171715'} color={'#9a8866'} borderColor={'#9a8866'} borderRadius={0} w={'150px'} fontSize={'14px'}>
-            <option value={'맵 선택'}>맵 선택</option>
-            <option value={'전체'}>전체</option>
-            <option value={'커스텀'}>커스텀</option>
-            <option value={'팩토리'}>팩토리</option>
-            <option value={'우드'}>우드</option>
-            <option value={'스트리트'}>스트리트</option>
-            <option value={'쇼어라인'}>쇼어라인</option>
-            <option value={'연구실'}>연구실</option>
-        </Select>
-    )
+     return (
+         <Select onChange={(e) => setMap(e.target.value)} fontWeight={'bold'} bgColor={'#171715'} color={'#9a8866'} borderColor={'#9a8866'} borderRadius={0} w={'150px'} fontSize={'14px'}>
+             <option value={'Select map'}>Select map</option>
+             <option value={'All'}>All</option>
+             <option value={'custom'}>Custom</option>
+             <option value={'Factory'}>Factory</option>
+             <option value={'Wood'}>Wood</option>
+             <option value={'Street'}>Street</option>
+             <option value={'Shoreline'}>Shoreline</option>
+             <option value={'Lab'}>Laboratory</option>
+         </Select>
+     )
 }
