ac7ce69d8018ccc4d967ae178cbfb0d3b78830d3
Hellfurian <Tyler.Long1228@gmail.com>
2023-10-06 11:01:50 -0400

    Update PlayTypeSelector.jsx
    Added filtering function

--- src/components/PlayTypeSelector.jsx
+++ src/components/PlayTypeSelector.jsx
@@ -1,30 +1,29 @@
-
 import { Select } from "@chakra-ui/react";
 import useFilterStore from "../store/useFilterStore";
 
 export default function PlayTypeSelector ({setter, isFilter=false}) {
 
-    const {setType} = useFilterStore()
+     const {setType} = useFilterStore()
 
-    if (!isFilter) {
-        return (
-            <Select onChange={setter} fontWeight={'bold'} bgColor={'#171715'} color={'#9a8866'} borderColor={'#9a8866'} borderRadius={0} w={'150px'} fontSize={'14px'}>
-                <option value={'플레이 유형'}>플레이 유형</option>
-                <option value={'파밍'}>파밍</option>
-                <option value={'퀘스트'}>퀘스트</option>
-                <option value={'교전'}>교전</option>
-                <option value={'보스런'}>보스런</option>
-            </Select>
-        )
-    }
+     if (!isFilter) {
+         return (
+             <Select onChange={setter} fontWeight={'bold'} bgColor={'#171715'} color={'#9a8866'} borderColor={'#9a8866'} borderRadius={0} w={'150px'} fontSize={'14px'}>
+                 <option value={'Play Type'}>Play Type</option>
+                 <option value={'Farming'}>Farming</option>
+                 <option value={'Quest'}>Quest</option>
+                 <option value={'Engage'}>Engage</option>
+                 <option value={'Boss Run'}>Boss Run</option>
+             </Select>
+         )
+     }
 
-    return (
-        <Select onChange={(e) => setType(e.target.value)} fontWeight={'bold'} bgColor={'#171715'} color={'#9a8866'} borderColor={'#9a8866'} borderRadius={0} w={'150px'} fontSize={'14px'}>
-                <option value={'플레이 유형'}>플레이 유형</option>
-                <option value={'파밍'}>파밍</option>
-                <option value={'퀘스트'}>퀘스트</option>
-                <option value={'교전'}>교전</option>
-                <option value={'보스런'}>보스런</option>
-        </Select>
-    )
+     return (
+         <Select onChange={(e) => setType(e.target.value)} fontWeight={'bold'} bgColor={'#171715'} color={'#9a8866'} borderColor={'#9a8866'} borderRadius={0} w={'150px'} fontSize={'14px'}>
+                 <option value={'Play Type'}>Play Type</option>
+                 <option value={'Farming'}>Farming</option>
+                 <option value={'Quest'}>Quest</option>
+                 <option value={'Engage'}>Engage</option>
+                 <option value={'Boss Run'}>Boss Run</option>
+         </Select>
+     )
 }
