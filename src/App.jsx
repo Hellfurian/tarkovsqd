e4d44bf73396f602d9800f5facf4abd6a694412d
Hellfurian <Tyler.Long1228@gmail.com>
2023-10-06 11:13:37 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,53 +11,53 @@ import useStatusStore from "./store/useStatusStore";
 
 function App() {
   
-  const {setPosts }           = usePostStore()
-  const {setOnline}           = useStatusStore()
+   const {setPosts } = usePostStore()
+   const {setOnline} = useStatusStore()
 
-  const sendNoti = (data) => {
-    if (Notification.permission !== 'granted') {
-      return
-    }
-    else {
-        const notification = new Notification(data.memo, {
-            body: '[디스코드] '  + data.nickname + '  ' + '[맵] ' + data.map + '  ' + '[서버] ' + data.server,
-        });
+   const sendNoti = (data) => {
+     if (Notification.permission !== 'granted') {
+       return
+     }
+     else {
+         const notification = new Notification(data.memo, {
+             body: '[Discord] ' + data.nickname + ' ' + '[Map] ' + data.map + ' ' + '[Server] ' + data.server,
+         });
 
-        notification.onclick = function () {
-            window.open(WEB);
-        };
-    }
-  }
+         notification.onclick = function () {
+             window.open(WEB);
+         };
+     }
+   }
   
-  useEffect(() => {
-    if (window.Notification) {
-      Notification.requestPermission();
-    }
+   useEffect(() => {
+     if (window.Notification) {
+       Notification.requestPermission();
+     }
 
-    const ws = new WebSocket(WEB_SOCKET)
-    ws.onmessage = (message) => {
-      const packet = JSON.parse(message.data)
-      if (packet.type === "UPDATE") { 
-        console.log("UPDATE PACKET TIME:" )
-        setPosts(packet.data) 
-        sendNoti(packet.data)
-      }
-      if (packet.type === "ONLINE") { setOnline(packet.data) }
-    }
+     const ws = new WebSocket(WEB_SOCKET)
+     ws.onmessage = (message) => {
+       const packet = JSON.parse(message.data)
+       if (packet.type === "UPDATE") {
+         console.log("UPDATE PACKET TIME:" )
+         setPosts(packet.data)
+         sendNoti(packet.data)
+       }
+       if (packet.type === "ONLINE") { setOnline(packet.data) }
+     }
 
-    return () => {
-      ws.close()
-    }
-  }, [])
+     return () => {
+       ws.close()
+     }
+   }, [])
 
-  return (
-    <Box w={'100%'} >
-      <Routes>
-        <Route path="/"        element={<Main />} />
-        <Route path="/discord" element={<Callback />} />
-      </Routes>
-    </Box>
-  );
+   return (
+     <Box w={'100%'} >
+       <Routes>
+         <Route path="/" element={<Main />} />
+         <Route path="/discord" element={<Callback />} />
+       </Routes>
+     </Box>
+   );
 }
 
 export default App;
