85b131b17041fe5289d6326d0fe4c25dea357fdb
Hellfurian <Tyler.Long1228@gmail.com>
2023-10-06 11:06:32 -0400

    Update TeamCreateModal.jsx
    Add play type

--- src/components/TeamCreateModal.jsx
+++ src/components/TeamCreateModal.jsx
@@ -1,19 +1,19 @@
 import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalBody,
-    ModalCloseButton,
-    VStack,
-    Input,
-    FormControl,
-    FormLabel,
-    HStack,
-    Text,
-    Button,
-    useToast
-  } from '@chakra-ui/react'
+     modal,
+     ModalOverlay,
+     ModalContent,
+     ModalHeader,
+     ModalBody,
+     ModalCloseButton,
+     VStack,
+     Input,
+     FormControl,
+     Form Label,
+     HStack,
+     Text,
+     Button,
+     useToast
+   } from '@chakra-ui/react'
 import { useState } from 'react'
 import MapSelector from './MapSelector'
 import RegionSelector from './RegionSelector'
@@ -24,137 +24,137 @@ import PlayTypeSelector from './PlayTypeSelector'
 
 export default function TeamCreateModal ({onClose, isOpen}) {
 
-    const profile = JSON.parse(localStorage.getItem('squadObject'))
+     const profile = JSON.parse(localStorage.getItem('squadObject'))
     
-    const toast = useToast()
+     const toast = useToast()
 
-    const [nickname, setNickname] = useState("")
-    const [map, setMap]           = useState("")
-    const [server, setServer]     = useState("")
-    const [type, setType]         = useState("")
-    const [memo, setMemo]         = useState("")
+     const [nickname, setNickname] = useState("")
+     const [map, setMap] = useState("")
+     const [server, setServer] = useState("")
+     const [type, setType] = useState("")
+     const [memo, setMemo] = useState("")
 
-    const validate = () => {
-        if (map === "" || map === "맵 선택") {
-            toast({
-                title: '맵을 선택해주세요',
-                status: 'warning'
-            })
-            return false
-        }
+     const validate = () => {
+         if (map === "" || map === "Select map") {
+             toast({
+                 title: 'Please select a map',
+                 status: 'warning'
+             })
+             return false
+         }
 
-        if (type === "" || type === "플레이 유형") {
-            toast({
-                title: '플레이 유형을 선택해주세요',
-                status: 'warning'
-            })
-            return false
-        }
+         if (type === "" || type === "Play Type") {
+             toast({
+                 title: 'Please select a play type',
+                 status: 'warning'
+             })
+             return false
+         }
 
-        if (server === "" || server === "서버 선택") {
-            toast({
-                title: '서버를 선택해주세요',
-                status: 'warning'
-            })
-            return false
-        }
+         if (server === "" || server === "Select Server") {
+             toast({
+                 title: 'Please select a server',
+                 status: 'warning'
+             })
+             return false
+         }
 
-        if (profile === null && nickname === "") {
-            toast({
-                title: '디스코드 아이디를 입력해주세요',
-                status: 'warning'
-            })
-            return false
-        }
+         if (profile === null && nickname === "") {
+             toast({
+                 title: ‘Please enter your Discord ID’,
+                 status: 'warning'
+             })
+             return false
+         }
 
-        if (memo === null || memo === "") {
-            toast({
-                title: '메모를 적어주세요',
-                status: 'warning'
-            })
-            return false
-        }
+         if (memo === null || memo === "") {
+             toast({
+                 title: 'Please write a note',
+                 status: 'warning'
+             })
+             return false
+         }
     
-        return true
-    }
+         return true
+     }
 
-    const customClose = () => {
-        setNickname('')
-        setMap('')
-        setServer('')
-        setType('')
-        setMemo('')
-        onClose()
-    }
+     const customClose = () => {
+         setNickname('')
+         setMap('')
+         setServer('')
+         setType('')
+         setMemo('')
+         onClose()
+     }
 
-    const sendCreatePacket = () => {
-        if (validate()) {
-            const packet = profile !== null ?
-            {
-                token: profile.token,
-                map,
-                server,
-                memo,
-                type,
-            } 
-            : {
-                nickname,
-                map,
-                server,
-                memo,
-                type,
-            }
+     const sendCreatePacket = () => {
+         if (validate()) {
+             const packet = profile !== null ?
+             {
+                 token: profile.token,
+                 map,
+                 server,
+                 memo,
+                 type,
+             }
+             : {
+                 nickname
+                 map,
+                 server,
+                 memo,
+                 type,
+             }
 
-            axios.post(API_SERVER + '/post', JSON.stringify(packet), {headers: {'Content-Type': 'application/json'}})
-            .then(() => {
-                customClose()
-            })
-            return
-        }
-    }
+             axios.post(API_SERVER + '/post', JSON.stringify(packet), {headers: {'Content-Type': 'application/json'}})
+             .then(() => {
+                 customClose()
+             })
+             return
+         }
+     }
     
-    return (
-        <Modal onClose={customClose} isOpen={isOpen} isCentered>
-            <ModalOverlay />
-            <ModalContent bgColor={'#121211'}p={'12px 0 12px 0'}>
-                <ModalHeader borderBottom={'1px solid #202020'} pt={3} pb={4} fontSize={'16px'} letterSpacing={'-1px'}>같이할 사람 찾기</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody pt={5}>
-                    <VStack alignItems={'flex-start'} spacing={8}>
-                        <FormControl isRequired>
-                            <FormLabel color={'#AEAEB0'} fontSize={'14px'}>디스코드 아이디</FormLabel>
-                            <HStack spacing={4}>
-                                {profile && <>
-                                    <Input fontSize={'14px'} p={'12px'} borderColor={'#827357'} value={profile.username} w={'180px'} disabled/> 
-                                    <Text color={'#5865f2'} fontSize={'14px'} letterSpacing={'-1px'}>스쿼드와 디스코드가 연동되어있어요!</Text>
-                                </>}
-                                {!profile && <>
-                                    <Input fontSize={'14px'} p={'12px'} borderColor={'#827357'} onChange={(e) => setNickname(e.target.value)} value={nickname} w={'180px'} placeholder='디스코드 아이디' /> 
-                                </>}
-                            </HStack>
-                        </FormControl>
-                        <HStack spacing={5}>
-                            <FormControl>
-                                <FormLabel color={'#AEAEB0'} fontSize={'14px'}>맵</FormLabel>
-                                <MapSelector      setter={(e) => setMap(e.target.value)} /> 
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel color={'#AEAEB0'} fontSize={'14px'}>플레이 유형</FormLabel>
-                                <PlayTypeSelector setter={(e) => setType(e.target.value)} />
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel color={'#AEAEB0'} fontSize={'14px'}>서버</FormLabel>
-                                <RegionSelector   setter={(e) => setServer(e.target.value)} />
-                            </FormControl>
-                        </HStack>
-                        <FormControl>
-                            <FormLabel color={'#AEAEB0'} fontSize={'14px'}>메모</FormLabel>
-                            <Input onChange={(e) => setMemo(e.target.value)} value={memo} fontSize={'14px'} p={'12px'} borderColor={'#827357'} placeholder={'랩 가서 떡상할 사람~'} />
-                        </FormControl>
-                        <Button onClick={sendCreatePacket} w={'100%'} fontSize={'15px'} borderRadius={0} bgColor={'#827357'} letterSpacing={'-1px'}>등록하기</Button>
-                    </VStack>
-                </ModalBody>
-            </ModalContent>
-        </Modal>
-    )
+     return (
+         <Modal onClose={customClose} isOpen={isOpen} isCentered>
+             <ModalOverlay />
+             <ModalContent bgColor={'#121211'}p={'12px 0 12px 0'}>
+                 <ModalHeader borderBottom={'1px solid #202020'} pt={3} pb={4} fontSize={'16px'} letterSpacing={'-1px'}>Find someone to join</ModalHeader>
+                 <ModalCloseButton />
+                 <ModalBody pt={5}>
+                     <VStack alignItems={'flex-start'} spacing={8}>
+                         <FormControl isRequired>
+                             <FormLabel color={'#AEAEB0'} fontSize={'14px'}>Discord ID</FormLabel>
+                             <HStack spacing={4}>
+                                 {profile && <>
+                                     <Input fontSize={'14px'} p={'12px'} borderColor={'#827357'} value={profile.username} w={'180px'} disabled/>
+                                     <Text color={'#5865f2'} fontSize={'14px'} letterSpacing={'-1px'}>Squad and Discord are linked!</Text>
+                                 </>}
+                                 {!profile && <>
+                                     <Input fontSize={'14px'} p={'12px'} borderColor={'#827357'} onChange={(e) => setNickname(e.target.value)} value={nickname} w={' 180px'} placeholder='Discord ID' />
+                                 </>}
+                             </HStack>
+                         </FormControl>
+                         <HStack spacing={5}>
+                             <FormControl>
+                                 <FormLabel color={'#AEAEB0'} fontSize={'14px'}>Map</FormLabel>
+                                 <MapSelector setter={(e) => setMap(e.target.value)} />
+                             </FormControl>
+                             <FormControl>
+                                 <FormLabel color={'#AEAEB0'} fontSize={'14px'}>Play Type</FormLabel>
+                                 <PlayTypeSelector setter={(e) => setType(e.target.value)} />
+                             </FormControl>
+                             <FormControl>
+                                 <FormLabel color={'#AEAEB0'} fontSize={'14px'}>Server</FormLabel>
+                                 <RegionSelector setter={(e) => setServer(e.target.value)} />
+                             </FormControl>
+                         </HStack>
+                         <FormControl>
+                             <FormLabel color={'#AEAEB0'} fontSize={'14px'}>Memo</FormLabel>
+                             <Input onChange={(e) => setMemo(e.target.value)} value={memo} fontSize={'14px'} p={'12px'} borderColor={'#827357'} placeholder={' Someone who will go to rap and have a good time~'} />
+                         </FormControl>
+                         <Button onClick={sendCreatePacket} w={'100%'} fontSize={'15px'} borderRadius={0} bgColor={'#827357'} letterSpacing={'-1px'}>Register</Button>
+                     </VStack>
+                 </ModalBody>
+             </ModalContent>
+         </Modal>
+     )
 }
